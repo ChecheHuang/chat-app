7022a0e755569291927257856e0b04217e900ce0
Carl <qwerfcxzas1@gmail.com>
2022-10-23 19:11:01 +0800

    test

--- client/src/page/Chat/Chat.jsx
+++ client/src/page/Chat/Chat.jsx
@@ -6,16 +6,28 @@ import { useSelector } from 'react-redux'
 function Chat() {
   const { user } = useSelector((state) => state.user)
   const scrollRef = useRef()
-  const [socket, setSocket] = useState(null)
+  // const [socket, setSocket] = useState(null)
   // useEffect(() => {
   //   setSocket(io(ws))
   // }, [])
   // useEffect(() => {
-  //     socket?.emit('addUser', user)
-  //     socket?.on('getUsers', (users) => {
-  //       console.log(users)
-  //     })
+  //   socket?.emit('addUser', user)
+  //   socket?.on('getUsers', (users) => {
+  //     console.log(users)
+  //   })
   // }, [socket])
+
+  const [socket, setSocekt] = useState(null)
+
+  useEffect(() => {
+    setSocekt(io('ws://localhost:5000'))
+  }, [])
+  useEffect(() => {
+    socket?.emit('addUser', "test")
+    socket?.on('user', (members) => {
+      console.log(members)
+    })
+  }, [socket])
 
   return (
     <div className="chat">
@@ -23,16 +35,13 @@ function Chat() {
         <div>Carl</div>
         <div>Mary</div>
         <div>Carl</div>
-        
       </div>
       <div className="messageArea">
         <div className="header">
           <div className="name">{user}</div>
           <button>登出</button>
         </div>
-        <div className="conversation">
-          
-        </div>
+        <div className="conversation"></div>
       </div>
     </div>
   )
--- server.js
+++ server.js
@@ -17,26 +17,44 @@ const io = require("socket.io")(http, {
 });
 
 let users = [];
-const addUser = (user, socketId) => {
-  !users.some((user) => user.user === user) && users.push({ user, socketId });
-};
-const removeUser = (socketId) => {
-  users = users.filter((user) => user.socketId !== socketId);
-};
+// const addUser = (user, socketId) => {
+//   !users.some((user) => user.user === user) && users.push({ user, socketId });
+// };
+// const removeUser = (socketId) => {
+//   users = users.filter((user) => user.socketId !== socketId);
+// };
+// io.on("connection", (socket) => {
+//   console.log("a user connected  " + socket.id);
+//   socket.on("addUser", (user) => {
+//     addUser(user, socket.id);
+//     // socket.emit("getUsers", users);
+//     socket.emit("getUsers", "有人進來拉");
+//   });
+
+//   socket.on("disconnect", (e) => {
+//     console.log("user disconnected ", socket.id);
+//     removeUser(socket.id);
+//     socket.emit("getUsers", users);
+//   });
+// });
+
 io.on("connection", (socket) => {
   console.log("a user connected  " + socket.id);
 
-  socket.on("addUser", (user) => {
-    addUser(user, socket.id);
-    socket.emit("getUsers", users);
+  socket.broadcast.emit("user", "test2");
+  //server on 一個事件
+  socket.on("addUser", (msg) => {
+    console.log(msg);
+  socket.emit("user", "test3");
+
   });
- 
+
   socket.on("disconnect", (e) => {
     console.log("user disconnected ", socket.id);
-    removeUser(socket.id);
-    socket.emit("getUsers", users);
   });
 });
+
+
 
 let apiRouter = require("./routes/api");
 app.use("/api", apiRouter);
