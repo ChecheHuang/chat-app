143f7d55a1942b1cfa394543a95432a1f40e5d09
Carl <carlhuang@sunnygo.com.tw>
2022-10-25 17:04:44 +0800

    modify

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,19 @@ const express = require("express");
 const router = express.Router();
 const {register,login,logout} =require('../controller/usersController')
 const {getMessages,sendMessage} =require('../controller/messagesController')
+
+router.post('/webhook',(req,res)=>{
+    const exec = require('child_process').exec;
+    console.log("打webhook")
+    const script=exec("sh webhook.sh",(error,stdout,stderr)=>{
+        console.log(stdout)
+        console.log(stderr)
+    })
+    if(error!==null){
+        console.log(error)
+    }
+    res.send("我是webhook!")
+})
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", logout);
