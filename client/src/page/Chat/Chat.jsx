dbf87d9db4806ed56a3679050f904698b89d242b
Carl <qwerfcxzas1@gmail.com>
2022-10-26 22:29:39 +0800

    modify

--- client/src/page/Chat/Chat.jsx
+++ client/src/page/Chat/Chat.jsx
@@ -38,11 +38,11 @@ function Chat() {
     })
     socket?.on('getMessage', (item) => {
       console.log(item)
-      if (item.sender === receiver) {
-        setMessages((prev) => {
-          return [...prev, item]
-        })
-      }
+      console.log('item.sender', item.sender)
+      console.log('receiver', receiver)
+       setMessages((prev) => {
+         return [...prev, item]
+       })
     })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [socket])
