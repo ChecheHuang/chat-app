cb5c30ad7e222cea438fe22f6d918f498517a776
Carl <carlhuang@sunnygo.com.tw>
2022-10-26 17:11:56 +0800

    moidfy

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,11 +1,11 @@
 const production = process.env.REACT_APP_PRODUCTION === 'true'
 const domain = production ? 'localhost' : 'cheche.ga'
-const host = `http://${domain}:8080`
+const port = production ? '3000' : '80'
+const host = `http://${domain}:${port}`
 export const registerRoute = `${host}/api/register`
 export const loginRoute = `${host}/api/login`
 export const logoutRoute = `${host}/api/logout`
 export const sendMessageRoute = `${host}/api/sendMessage`
 export const getMessagesRoute = `${host}/api/getMessages`
 
-export const ws = `ws://${domain}:8080`
-// export const ws = `ws://${domain}:8080`
+export const ws = `ws://${domain}:${port}`
