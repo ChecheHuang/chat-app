b45d4c39fafb67e8174c3f939bbf23237d14805c
Carl <carlhuang@sunnygo.com.tw>
2022-10-26 16:56:19 +0800

    modify

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,12 +1,11 @@
 const production = process.env.REACT_APP_PRODUCTION === 'true'
 const domain = production ? 'localhost' : 'cheche.ga'
-const host = `http://${domain}:80`
-// const host = `http://${domain}:8080`
+const host = `http://${domain}:8080`
 export const registerRoute = `${host}/api/register`
 export const loginRoute = `${host}/api/login`
 export const logoutRoute = `${host}/api/logout`
 export const sendMessageRoute = `${host}/api/sendMessage`
 export const getMessagesRoute = `${host}/api/getMessages`
 
-export const ws = `ws://${domain}:80`
+export const ws = `ws://${domain}:8080`
 // export const ws = `ws://${domain}:8080`
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 
 const io = require("socket.io")(http, {
   cors: {
-    origin:process.env.PRODUCTION? "http://localhost:3000":"http://cheche.ga:80",
+    origin:process.env.PRODUCTION? "http://localhost:3000":"http://cheche.ga:8080",
   },
 });
 const socket = require("./controller/messagesController");
