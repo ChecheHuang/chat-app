bde4753be423edce4adde7330cb7d3d548641497
Carl <carlhuang@sunnygo.com.tw>
2022-10-26 16:49:02 +0800

    modify

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,10 +1,12 @@
 const production = process.env.REACT_APP_PRODUCTION === 'true'
 const domain = production ? 'localhost' : 'cheche.ga'
-const host = `http://${domain}:8080`
+const host = `http://${domain}`
+// const host = `http://${domain}:8080`
 export const registerRoute = `${host}/api/register`
 export const loginRoute = `${host}/api/login`
 export const logoutRoute = `${host}/api/logout`
 export const sendMessageRoute = `${host}/api/sendMessage`
 export const getMessagesRoute = `${host}/api/getMessages`
 
-export const ws = `ws://${domain}:8080`
+export const ws = `ws://${domain}`
+// export const ws = `ws://${domain}:8080`
